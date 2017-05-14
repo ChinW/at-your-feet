cec79a3746cd43034fc13d37d4c2ec9f366cefe1
Chi <contractswif@hotmail.com>
2017-05-14 09:55:53 +0800

    add setting page

--- app/components/MainSection.js
+++ app/components/MainSection.js
@@ -4,6 +4,7 @@ const { Header, Content, Footer } = Layout;
 import '../assets/global.scss';
 import TodayPage from './Today'
 import HistoryPage from './History'
+import Setting from './Setting'
 
 export default class MainSection extends Component {
   static propTypes = {
@@ -18,10 +19,13 @@ export default class MainSection extends Component {
   switchPage() {
       switch(this.props.mode) {
           case 0:
-          return <TodayPage />;
+                return <TodayPage />;
           break;
           case 1:
-          return <HistoryPage />;
+                return <HistoryPage />;
+                break;
+          case 2:
+              return <Setting />;
           break;
     }
   }
