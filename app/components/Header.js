aeca3f23c6557c5d65727839d2d3fc5bb848b122
Chi <contractswif@hotmail.com>
2017-05-14 09:48:04 +0800

    no message

--- app/components/Header.js
+++ app/components/Header.js
@@ -2,6 +2,7 @@ import React, { PropTypes, Component } from 'react';
 import { Layout, Menu, Breadcrumb } from 'antd';
 const { Header, Content } = Layout;
 
+const logo = require('../../chrome/assets/img/icon-128.png')
 export default class AYTHeader extends Component {
 
   handleSave = (text) => {
@@ -21,6 +22,9 @@ export default class AYTHeader extends Component {
                 defaultSelectedKeys={['0']}
                 style={{ lineHeight: '64px' }}
             >
+                <div key="logo" className="logo-menu">
+                    <img src={logo} alt=""/>
+                </div>
                 <Menu.Item key="0" >今日</Menu.Item>
                 <Menu.Item key="1" >自定义/历史</Menu.Item>
                 <Menu.Item key="2" >设置</Menu.Item>
